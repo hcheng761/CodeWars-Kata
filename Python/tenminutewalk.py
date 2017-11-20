e5d61edd3ea60172492bd68041a716b4278f30c2
hcheng761 <hcheng761@gmail.com>
2017-11-19 18:43:24 -0600

    Take a Ten Minute (10/2)

---
+++ Python/tenminutewalk.py
@@ -0,0 +1,22 @@
+def isValidWalk(walk):
+    #determine if walk is valid
+    if len(walk) < 10:
+        return False
+    else:
+        updown = 0
+        leftright = 0
+        
+        for i in walk:
+            if i=='n':
+                updown+=1
+            elif i=='s':
+                updown-=1
+            elif i=='w':
+                leftright+=1
+            elif i=='e':
+                leftright-=1
+        if updown==0 and leftright==0:
+            return True
+        else:
+            return False
+    pass

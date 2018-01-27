9d497366416d70b214591439a26ff033e193f6ad
hcheng761 <hcheng761@gmail.com>
2018-01-27 14:48:57 -0600

    Count of positives / sum of negatives (1/20)

---
+++ C++/pos&neg.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+std::vector<int> countPositivesSumNegatives(std::vector<int> input)
+{
+    if(input.size() == 0)
+      return std::vector<int>();
+      
+    int count = 0;
+    int sum = 0;
+    
+    for(int i = 0;i<input.size();i++)
+    {
+      if(input[i] > 0)
+        count += 1;
+      if(input[i] < 0)
+        sum += input[i];
+    }
+    return std::vector<int> {count,sum};
+}
